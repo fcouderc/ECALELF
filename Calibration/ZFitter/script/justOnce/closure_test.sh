bd60436bd6c0032fd38daeb33941deb4e89723fe
gfasanel <giuseppe.fasanella@cern.ch>
2015-07-10 22:51:42 +0200

    cleaning

--- Calibration/ZFitter/script/justOnce/closure_test.sh
+++ Calibration/ZFitter/script/justOnce/closure_test.sh
@@ -1,68 +1,69 @@
 #!/bin/bash
+# inspired from ZFitter/script/kustOnce/smearing.sh
 queue=$1
-nEventsPerToy=factorizedSherpaFixed_DataSeedFixed_smooth_autobin_smearingEt_${queue}
-#nEventsPerToy=factorized
+nEventsPerToy=closure #I think it's just a name
 nSmearToy=30 #it quantifies the over-sampling
 commonCut=Et_25-trigger-noPF
 
 if [ -z "${queue}" ];then local=y; fi
 
-for regionsFile in scaleStep2smearing_1
-  do
-  dir=test/dato/fitres/toys/${regionsFile}
-
-for scale in 1.00 1.01 0.99 1.02 0.98 1.05 0.95
+for regionsFile in scaleStep0
 do
-for constAlpha in  0.02-0.00 0.04-0.00 0.01-0.00 
-  do
-  const=`echo $constAlpha | cut -d '-' -f 1`
-  alpha=`echo $constAlpha | cut -d '-' -f 2`
+    dir=test/dato/fitres/toys/${regionsFile}
 
-  echo "[[[[[[[[[[[ const:alpha ]]]]]]]]]]] ${const}:${alpha}"
-  baseDir=${dir}/${nEventsPerToy}/${const}-${alpha}-${scale}/
-  mkdir -p $baseDir 
+    for scale in 1.00 #1.01 0.99 1.02 0.98 1.05 0.95
+    do
+	for constAlpha in  0.00-0.00 #0.04-0.00 0.01-0.00 
+	do
+	    const=`echo $constAlpha | cut -d '-' -f 1`
+	    alpha=`echo $constAlpha | cut -d '-' -f 2`
+	    
+	    echo "[[[[[[[[[[[ const:alpha ]]]]]]]]]]] ${const}:${alpha}"
+	    baseDir=${dir}/${nEventsPerToy}/${const}-${alpha}-${scale}/
+	    mkdir -p $baseDir 
 
-  cat data/validation/toyMC-smearingMethod.dat > ${baseDir}/invertedToyMC.dat
-
-#mcToy.txt is the initFile passed to ZFitter
-  cat > $baseDir/${alphaConst}/mcToy.txt <<EOF
-scale_absEta_1_1.4442-gold-${commonCut} 	=  ${scale} +/- 0.0050000 L(0.9 - 1.1) // [GeV]
+            #mcToy.txt is the initFile passed to ZFitter
+	    cat > $baseDir/${alphaConst}/mcToy.txt <<EOF
+constTerm_EB_invMass_100_2000_${commonCut} = ${const} +/- 0.139700 L(0 - 0.2)
+constTerm_EE_invMass_100_2000_${commonCut} = ${const} +/- 0.218710 L(0 - 0.2)
+scale_EB_invMass_100_2000_${commonCut} = ${scale} +/- 0.000415 L(0.96 - 1.04)
+scale_EE_invMass_100_2000_${commonCut} = ${scale} +/- 0.000609 L(0.96 - 1.04)
 EOF
 
 
-  cat data/validation/toyMC-smearingMethod.dat > ${baseDir}/toyMC.dat
+	    cat data/validation/22Jan2012-runDepMCAll_checkMee.dat > ${baseDir}/toyMC.dat
 
-  for nToys in `seq 1 100`; 
-  do 
-      newDir=${baseDir}/${alphaConst}/${nSmearToy}/${nToys}/
-      mkdir -p $newDir 
-      ls $newDir
+	    for nToys in `seq 1 100`; 
+	    do 
+		newDir=${baseDir}/${alphaConst}/${nSmearToy}/${nToys}/
+		mkdir -p $newDir 
+		ls $newDir
       
-      if [ -z "${local}" ];then
-	  bsub -q ${queue} -oo ${newDir}/stdout.log -eo ${newDir}/stderr.log -J "$regionsFile-$const-$alpha" \
-	      "cd /afs/cern.ch/user/g/gfasanel/new_version_ECALELF/CMSSW_7_5_0_pre4/src/Calibration/ZFitter; eval \`scramv1 runtime -sh\`; uname -a;  echo \$CMSSW_VERSION; ./bin/ZFitter.exe -f ${baseDir}/toyMC.dat \
+		if [ -z "${local}" ];then
+		    bsub -q ${queue} -oo ${newDir}/stdout.log -eo ${newDir}/stderr.log -J "$regionsFile-$const-$alpha" \
+			"cd /afs/cern.ch/user/g/gfasanel/new_version_ECALELF/CMSSW_7_5_0_pre4/src/Calibration/ZFitter; eval \`scramv1 runtime -sh\`; uname -a;  echo \$CMSSW_VERSION; ./bin/ZFitter.exe -f ${baseDir}/toyMC.dat \
       --regionsFile=data/regions/${regionsFile}.dat \
 	--commonCut=${commonCut} --smearerFit --outDirFitResData=$newDir \
         --constTermFix --alphaGoldFix --smearerType=profile --noPU --smearingEt \
 	--initFile=${baseDir}/${alphaConst}/mcToy.txt --profileOnly --plotOnly --runToy --nSmearToy=${nSmearToy} \
         --autoBin        > ${newDir}/log2.log"
-      else
-	  echo "#============================================================ Toy = $nToys"
-	  ./bin/ZFitter.exe -f ${baseDir}/toyMC.dat \
-	      --regionsFile=data/regions/${regionsFile}.dat \
-	      --commonCut=${commonCut} --smearerFit --outDirFitResData=$newDir \
-	      --constTermFix --smearerType=profile --noPU  --smearingEt \
-	      --initFile=${baseDir}/${alphaConst}/mcToy.txt \
-	      --plotOnly --profileOnly --runToy --eventsPerToy=0 --nSmearToy=${nSmearToy} --autoBin |tee ${newDir}/nSmearToy_${nSmearToy}.log
-	  exit 0
-      fi
+		else
+		    echo "#============================================================ Toy = $nToys"
+		    ./bin/ZFitter.exe -f ${baseDir}/toyMC.dat \
+			--regionsFile=data/regions/${regionsFile}.dat \
+			--commonCut=${commonCut} --smearerFit --outDirFitResData=$newDir \
+			--constTermFix --smearerType=profile --noPU  --smearingEt \
+			--initFile=${baseDir}/${alphaConst}/mcToy.txt \
+			--plotOnly --profileOnly --runToy --eventsPerToy=0 --nSmearToy=${nSmearToy} --autoBin |tee ${newDir}/nSmearToy_${nSmearToy}.log
+		    exit 0
+		fi
+	    done
+	    wait 
+	done
+	wait
     done
-    wait 
-  done
-  wait
 done
 
-done
 
 exit 0
 
