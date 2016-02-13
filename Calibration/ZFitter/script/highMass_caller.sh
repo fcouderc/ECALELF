ab7c527c0f8b36e47f2752ed6a410a24d601bc26
gfasanel <giuseppe.fasanella@cern.ch>
2016-02-13 11:50:16 +0100

    caller High Mass: just example

--- Calibration/ZFitter/script/highMass_caller.sh
+++ Calibration/ZFitter/script/highMass_caller.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-./script/calibration_highMass.sh HighMass_study_2016_74 invMass_SC_corr reReco_Ele_regrCorr_highMass CatOnly #--initFile=step2/init_1_rereco.txt --initFile=step2/init_2_rereco.txt 
+./script/calibration_highMass.sh data/validation/HighMass_study_2016_74.dat invMass_SC_corr Init #--initFile=step2/init_1_rereco.txt --initFile=step2/init_2_rereco.txt 
 
 
 
